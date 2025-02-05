c3eac2eeea99c240e7ff5796e635d65193deeb81
Rubinsam11 <rubinsam052@gmail.com>
2025-02-05 22:18:15 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,14 +15,14 @@ const Profile = () => {
         </div>
         <div className="section__text">
           <p className="section__text__p1">Hello, I'm</p>
-          <h1 className="title"> RUBIN SAM</h1>
+          <h1 className="title">.      RUBIN SAM</h1>
           <p className="section__text__p2">
             W E B  &  F U L L- S T A C K  D E V E L O P E R  <br />I N T E R N
           </p>
           <div className="btn-container">
             <button
               className="btn btn-color-2"
-              onClick={() => window.open('./assets/resume.pdf')}
+              onClick={() => window.open('/resume.pdf', '_blank')}
             >
               Download CV
             </button>
